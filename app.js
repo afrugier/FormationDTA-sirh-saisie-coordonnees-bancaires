9ac79c88a00ab622d930718687312d89ee259347
Damien <dguironnet@gmail.com>
2017-08-04 17:37:01 +0200

    cleaned JS

--- app.js
+++ app.js
@@ -1,64 +1,103 @@
+var elementListeCollabs;
+var inputMatricule;
+var inputBanque;
+var inputBic;
+var inputIban;
+
+var buttonSubmit;
+
+var classCollabLine;
+var classMatricule;
+
 $(document).ready(function () {
+    initVariables();
+    initStaticEvents();
     init();
 });
 
-function init() {
-    $("#sauvegarderBtn").on("click", function (e) {
+function initVariables() {
+    inputMatricule = $("#matricule");
+    inputBanque = $("#banque");
+    inputBic = $("#bic");
+    inputIban = $("#iban");
+    elementListeCollabs = $("#listeCollabs");
+
+    buttonSubmit = $("#sauvegarderBtn");
+
+    classCollabLine = "collab";
+    classMatricule = "matricule";
+}
+
+function initStaticEvents() {
+    buttonSubmit.on("click", function (e) {
         e.preventDefault();
         postBankData();
     });
+}
+
+function init() {
     updateInputFields("", "", "");
     getListCollabs();
+    disableFields(true);
+}
+
+function populateTable(data) {
+    elementListeCollabs.find(classCollabLine).remove();
+    data.forEach(function (element) {
+        var line = "<tr class='" + classCollabLine + "'>";
+        line += "<td class='" + classMatricule + "'>" + element.matricule + "</td>";
+        line += "<td>" + element.nom + "</td>";
+        line += "<td>" + element.prenom + "</td>";
+        line += "</tr>";
+
+        elementListeCollabs.append(line);
+    }, this);
+
+    updateListeners();
+}
+
+function updateInputFields(bankData, matricule) {
+    inputBanque.val(bankData.banque);
+    inputBic.val(bankData.bic);
+    inputIban.val(bankData.iban);
+    inputMatricule.val(matricule);
+
+    disableFields(false);
+}
+
+function disableFields(state) {
+    inputBanque.prop("disabled", state);
+    inputBic.prop("disabled", state);
+    inputIban.prop("disabled", state);
+    buttonSubmit.prop("disabled", state);
+}
+
+function updateListeners() {
+    elementListeCollabs.find("." + classCollabLine).on("click", function () {
+        var matricule = $(this).find("." + classMatricule).text();
+
+        $.get("http://localhost:8080/sgp/api/collaborateurs/" + matricule + "/banque").done(function (collabBankData) {
+            updateInputFields(collabBankData, matricule);
+        });
+    });
 }
 
 function getListCollabs() {
     $.get("http://localhost:8080/sgp/api/collaborateurs").done(function (data) {
         populateTable(data);
     });
-};
+}
 
 function postBankData() {
     $.ajax({
-        url: "http://localhost:8080/sgp/api/collaborateurs/" + encodeURIComponent($("#matricule").val()) + "/banque?banque=" + encodeURIComponent($("#banque").val()) + "&bic=" + encodeURIComponent($("#bic").val()) + "&iban=" + $("#iban").val(),
+        url: "http://localhost:8080/sgp/api/collaborateurs/" + encodeURIComponent(inputMatricule.val()) + "/banque?banque=" + encodeURIComponent(inputBanque.val()) + "&bic=" + encodeURIComponent(inputBic.val()) + "&iban=" + inputIban.val(),
         type: 'PUT',
         success: function (data) {
-            alert("User with matricule [" + $("#matricule").val() + "] was updated");
+            alert("User with matricule [" + inputMatricule.val() + "] was updated");
             init();
         },
         error: function (XMLHttpRequest, textStatus, errorThrown) {
             alert("User could not be updated. Check your data");
         }
     });
-};
-
-function populateTable(data) {
-    $("#listeCollabs > .collab").remove();
-    data.forEach(function (element) {
-        var line = "<tr class='collab'>";
-        line += "<td class='matricule'>" + element.matricule + "</td>";
-        line += "<td>" + element.nom + "</td>";
-        line += "<td>" + element.prenom + "</td>";
-        line += "</tr>";
-
-        $("#listeCollabs").append(line);
-    }, this);
-
-    updateListeners();
-};
-
-function updateListeners() {
-    $("#listeCollabs > .collab").on("click", function () {
-        var matricule = $(this).find(".matricule").text();
-
-        $.get("http://localhost:8080/sgp/api/collaborateurs/" + matricule + "/banque").done(function (collabBankData) {
-            updateInputFields(collabBankData, matricule);
-        });
-    });
-};
-
-function updateInputFields(bankData, matricule) {
-    $("#banque").val(bankData.banque);
-    $("#bic").val(bankData.bic);
-    $("#iban").val(bankData.iban);
-    $("#matricule").val(matricule);
-};
+}
